9f2377987e3630c468a0138428df6f78c76ef130
Pablo Terán <pterancobo@gmail.com>
2015-05-19 16:32:41 +0200

    Initial state
    Just the original files

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function creates a Matrix Object
+# For simplicity, from now on, the matrix is referred to as "M", and its inverse, as "iM"
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(M = matrix()) {
+    iM <- NULL
+    # This function coerces M to be the given "y" matrix and resets the inverse matrtix "iM"
+    set <- function(y) {
+        M <<- y
+        iM <<- NULL
+    }
+    # This function simply returns the matrix M
+    get <- function() M
+    # This function coerces iM to be the given inverse matrix "inverse" 
+    setinverse <- function(inverse) iM <<- inverse
+    # This function simply returns the inverse matrix "iM"
+    getinverse <- function() iM
+    
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function calculates the inverse of the given Matrix Object
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(M, ...) {
+    # Return a matrix that is the inverse of 'M'
+    iM <- M$getinverse()
+    if(!is.null(iM)) {
+        message("getting cached data")
+        return(iM)
+    }
+    
+    # Get the matrix first
+    data <- M$get()
+    d <- dim(data)
+    # Check the matrix is square (not really requested but...)
+    if (d[1] != d[2]){
+        return(iM)
+    } 
+    # If the matrix is square, calculate the inverse
+    else{
+        # Create a diagonal matrix of the same dimension as M
+        unitM = diag(d[1])
+        iM <- solve(data,unitM)
+        M$setinverse(iM)
+        iM
+    }
 }
