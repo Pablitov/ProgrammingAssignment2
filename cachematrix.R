53642de96f0f21e8636378fa5f274fe9bd3778c5
Pablo Terán <pterancobo@gmail.com>
2015-05-19 15:50:32 +0200

    Comments to complete assignment 2"
    Comments introduced in cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,44 +3,13 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(M = matrix()) {
-    iM <- NULL
-    
-    set <- function(y) {
-        M <<- y
-        iM <<- NULL
-    }
-    
-    get <- function() M
-    
-    setinverse <- function(inverse) iM <<- inverse
-    
-    getinverse <- function() iM
-    
-    list(set = set, get = get,
-         setinverse = setinverse,
-         getinverse = getinverse)
+makeCacheMatrix <- function(x = matrix()) {
+
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(M, ...) {
+cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-    iM <- M$getinverse()
-    if(!is.null(iM)) {
-        message("getting cached data")
-        return(iM)
-    }
-    data <- M$get()
-    d <- dim(data)
-    if (d[1] != d[2]){
-        return(iM)
-    } 
-    else{
-        unitM = diag(d[1])
-        iM <- solve(data,unitM)
-        M$setinverse(iM)
-    }
-    iM    
 }
